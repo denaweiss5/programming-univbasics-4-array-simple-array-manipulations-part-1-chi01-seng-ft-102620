bb27155445805356611e20bfa9b6944e59de1eb6
Dena Neuman <denaweiss5@gmail.com>
2020-10-06 15:22:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,7 @@ end
 def using_pop(continents)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   continents.pop
+  continents= ["Africa", "Asia", "Europe", "North America", "South America", "Oceania"]
 end
 
 def pop_with_args (small_dogs)
